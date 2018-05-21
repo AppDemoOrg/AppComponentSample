183e35ec58757cbd011147d471ffd8df58292f28
hwq <weiqi.huang@pisofttech.com>
2018-05-21 18:48:59 +0800

    添加build-gradle

---
+++ build-gradle/src/main/groovy/com/abt/build_gradle/util/StringUtil.java
@@ -0,0 +1,16 @@
+package com.abt.build_gradle.util;
+
+import java.util.regex.Pattern;
+
+public class StringUtil {
+    /**
+     * 是否是maven 坐标
+     * @return
+     */
+    public static boolean isMavenArtifact(String str) {
+        if (str == null || str.isEmpty()) {
+            return false;
+        }
+        return Pattern.matches("\\S+(\\.\\S+)+:\\S+(:\\S+)?(@\\S+)?", str);
+    }
+}
